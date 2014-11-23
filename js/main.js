4d3df3323bbe6f87244ba3303616906347224fe4
david <deltac2008@googlemail.com>
2014-11-23 13:51:38 +0000

    updated code to include sliding page transition and config updated

--- js/main.js
+++ js/main.js
@@ -46,15 +46,21 @@ var app = {
 	},
     
 	route: function() {
+		var self = this;
 		var hash = window.location.hash;
 		if (!hash) {
-			$('body').html(new HomeView(this.store).render().el);
+			if (this.homePage) {
+				this.slidePage(this.homePage);
+			} else {
+				this.homePage = new HomeView(this.store).render();
+				this.slidePage(this.homePage);
+			}
 			return;
 		}
-		var match = hash.match(app.detailsURL);
+		var match = hash.match(this.detailsURL);
 		if (match) {
 			this.store.findById(Number(match[1]), function(employee) {
-				$('body').html(new EmployeeView(employee).render().el);
+				self.slidePage(new EmployeeView(employee).render());
 			});
 		}
 	},
@@ -103,8 +109,8 @@ var app = {
 		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.registerEvents();
 		this.store = new MemoryStore(function() {
-			//self.route();
-			self.renderHomeView();
+			self.route();
+			//self.renderHomeView();
 			//self.showAlert('Store Initialised', 'Info');
 		});
 		this.homeTpl = Handlebars.compile($("#home-tpl").html());
